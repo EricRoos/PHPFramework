3cf12efc6c9c1aceebae5fdfabe907889a1d0aba
Eric Roos <eric_roos@baylor.edu>
2013-10-26 22:41:39 -0500

    substisted two spaces for tab

--- config/user_router.php
+++ config/user_router.php
@@ -1,21 +1,21 @@
 <?php 
-	class UserRouter extends Router{
-		
-		public function __construct(){
-		parent::__construct();
-			$this->define_resource('members',array('index','new','show','create'));
-			$this->define_resource('comments',array('index','new','show','create'));
-		}
+  class UserRouter extends Router{
+  	
+  	public function __construct(){
+  	parent::__construct();
+  		$this->define_resource('members',array('index','new','show','create'));
+  		$this->define_resource('comments',array('index','new','show','create'));
+  	}
 
 
-		public function define_get(){
-		}
-		public function define_post(){
-		}
-		public function define_put(){
-		}
-		public function define_delete(){
-		}
+  	public function define_get(){
+  	}
+  	public function define_post(){
+  	}
+  	public function define_put(){
+  	}
+  	public function define_delete(){
+  	}
 
-	}
+  }
 ?>
