23f5b211e6783fbfc4341cbeb506d95172f1c7c9
eric <eric@ubuntu.(none)>
2013-10-28 18:53:43 -0500

    changed routes

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,7 @@
 	$path = $_SERVER['REQUEST_URI'];
 	$uri_tokens = explode('/',$path);
 	$requested_model = $uri_tokens[1];
+	echo $requested_model;
 	$root_extension = '/app/controllers/'.$requested_model.'_controller.php';
 	if(file_exists($_SERVER['DOCUMENT_ROOT'].$root_extension)){
 		require_once($_SERVER['DOCUMENT_ROOT'].$root_extension);
